803254c7e299481b68eff00743c76bd242c7c123
choiseongrak <csr961120@gmail.com>
2020-09-10 18:27:02 +0900

    0910_Practice

--- JSP/WORKSPACE/0907_Practice/WebContent/0907_lecture/parent.js
+++ JSP/WORKSPACE/0907_Practice/WebContent/0907_lecture/parent.js
@@ -4,7 +4,7 @@ const parBtn3 = document.querySelector('.parBtn3');
 
 
 let web;
-parBtn1.addEventListener('click', () => { web = window.open("child.html","_blank","width=300p, height=300px")});
+parBtn1.addEventListener('click', () => { web = window.open("child.html","_blank","width=300px, height=300px")});
 parBtn2.addEventListener('click', () => web.close());
 parBtn3.addEventListener('click', () => window.close());
 
